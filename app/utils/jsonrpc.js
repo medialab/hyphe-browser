515d1ce0ec28879c55b583ad76f6bc287b6642a4
Nicolas Chambrier <naholyr@gmail.com>
2016-01-11 15:55:16 +0100

    Remove crappy old-school dependency 'request'

--- app/utils/jsonrpc.js
+++ app/utils/jsonrpc.js
@@ -1,21 +1,17 @@
-import request from 'request'
-
 // Sample usage: jsonrpc('http://hyphe.medialab.sciences-po.fr/dev-forccast-api')('list_corpus')
-export default (uri) => (method, params = []) => new Promise((resolve, reject) => {
-  request.post(uri, {
-    form: JSON.stringify({
-      method,
-      params
-    })
-  }, (err, response, body) => {
-    if (err) {
-      reject(err)
+export default (uri) => (method, params = []) => (
+  fetch(uri, {
+    method: 'POST',
+    body: JSON.stringify({method, params})
+  })
+  .then((response) => response.json())
+  .then(([row]) => {
+    if (row.code === 'success') {
+      return row.result
+    } else if (row.code === 'fail') {
+      throw new Error(row.message)
     } else {
-      try {
-        resolve(JSON.parse(body))
-      } catch (e) {
-        reject(e)
-      }
+      throw new Error('Unrecognized response')
     }
   })
-})
+)
