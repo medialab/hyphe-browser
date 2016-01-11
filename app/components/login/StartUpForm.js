a004998b63e3945b025957f88da1fc2da8d36ce1
Delapouite <delapouite@gmail.com>
2016-01-11 10:26:42 +0100

    feat(components): add basic components for login page

---
+++ app/components/login/StartUpForm.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react'
+
+export default (props) => {
+  return (
+    <form className="start-up-form">
+      <h2>Welcome to Hyphe</h2>
+      <p>Please select a server instance:</p>
+    </form>
+  )
+}
