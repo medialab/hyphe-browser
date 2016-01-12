a82bf596a258c72d8f1ba04a8104c20c4731c3fe
Delapouite <delapouite@gmail.com>
2016-01-12 15:01:26 +0100

    feat(components): implement CorpusForm

--- app/actions/corpora.js
+++ app/actions/corpora.js
@@ -2,12 +2,18 @@ import jsonrpc from '../utils/jsonrpc'
 import { pushPath } from 'redux-simple-router'
 
 export const SELECT_CORPUS = '§_SELECT_CORPUS'
+
 export const FETCH_CORPUS_STATUS_REQUEST = '§_FETCH_CORPUS_STATUS_REQUEST'
 export const FETCH_CORPUS_STATUS_SUCCESS = '§_FETCH_CORPUS_STATUS_SUCCESS'
 export const FETCH_CORPUS_STATUS_FAILURE = '§_FETCH_CORPUS_STATUS_FAILURE'
+
 export const START_CORPUS_REQUEST = '§_START_CORPUS_REQUEST'
 export const START_CORPUS_SUCCESS = '§_START_CORPUS_SUCCESS'
 export const START_CORPUS_FAILURE = '§_START_CORPUS_FAILURE'
+
+export const CREATE_CORPUS_REQUEST = '§_CREATE_CORPUS_REQUEST'
+export const CREATE_CORPUS_SUCCESS = '§_CREATE_CORPUS_SUCCESS'
+export const CREATE_CORPUS_FAILURE = '§_CREATE_CORPUS_FAILURE'
 
 export const selectCorpus = (corpus) => (dispatch) => {
   dispatch({
@@ -50,4 +56,29 @@ export const startCorpus = (serverUrl, corpus, password) => (dispatch) => {
   return jsonrpc(serverUrl)('start_corpus', [corpus.corpus_id, password])
     .then(() => dispatch({ type: START_CORPUS_SUCCESS, payload: { corpus } }))
     .catch((error) => dispatch({ type: START_CORPUS_FAILURE, payload: { corpus, error } }))
+}
+
+export const receiveCorpus = (serverUrl, corpus) => ({
+  type: CREATE_CORPUS_SUCCESS,
+  payload: {
+    serverUrl,
+    corpus
+  }
+})
+
+export const createCorpus = (serverUrl, corpus) => (dispatch) => {
+  dispatch({
+    type: CREATE_CORPUS_REQUEST,
+    payload: {
+      serverUrl,
+      corpus
+    }
+  })
+
+  return jsonrpc(serverUrl)('create_corpus', corpus)
+    .then((res) => dispatch(receiveCorpus(serverUrl, res)))
+    .catch((err) => dispatch({
+      type: CREATE_CORPUS_FAILURE,
+      payload: err
+    }))
 }
--- app/components/login/CorpusForm.js
+++ app/components/login/CorpusForm.js
@@ -2,33 +2,142 @@
 
 import '../../css/login/corpus-form'
 
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
 
-export default (props) => {
+import * as actions from '../../actions/corpora'
 
-  return (
-    <form className="server-form">
-      <h2 className="pane-centered-title">Corpus edition</h2>
-      <div className="form-group">
-        <label>Corpus name</label>
-        <input className="form-control" />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input className="form-control" />
-      </div>
-      <div className="form-group">
-        <label>Confirm password</label>
-        <input className="form-control" />
-      </div>
-      <div className="form-actions">
-        <button className="btn btn-primary">Create corpus</button>
-        <Link className="btn btn-default" to="/login">Cancel</Link>
-      </div>
-    </form>
-  )
+class CorpusForm extends Component {
 
+  constructor (props) {
+    super(props)
+    this.state = this.getInitState()
+  }
+
+  onSubmit (evt) {
+    // no real submit to the server
+    evt.preventDefault()
+    this.setFormState('disabled', true)
+
+    if (!this.checkPassword()) {
+      this.setFormState('disabled', false)
+      this.setDataState('password', '')
+      this.setDataState('passwordConfirm', '')
+      // TODO error message
+      console.error('passwords must match')
+      return
+    }
+
+    let corpus = {
+      ...this.state.data
+    }
+
+    // clean unused info
+    delete corpus.passwordConfirm
+    if (!corpus.name) delete corpus.name
+    if (!corpus.password) delete corpus.password
+
+    this.props.actions.createCorpus(this.props.server.url, corpus)
+  }
+
+  onChangeName (evt) {
+    this.setDataState('name', evt.target.value)
+  }
+
+  onChangePassword (evt) {
+    this.setDataState('password', evt.target.value)
+  }
+
+  onChangePasswordConfirm (evt) {
+    this.setDataState('passwordConfirm', evt.target.value)
+  }
+
+  checkPassword () {
+    return this.state.data.password === this.state.data.passwordConfirm
+  }
+
+  getInitState () {
+    return {
+      // form state
+      disabled: false,
+      errors: [],
+      // fields values
+      data: {
+        name: null,
+        password: null,
+        passwordConfirm: null
+      }
+    }
+  }
+
+  // proxy for setState
+  setFormState (key, value) {
+    let state = {
+      ...this.state,
+      [key]: value
+    }
+    this.setState(state)
+  }
+
+  // deal with fields values
+  setDataState (key, value) {
+    let data = {
+      ...this.state.data,
+      [key]: value
+    }
+    this.setFormState('data', data)
+  }
+
+  render () {
+    const { server } = this.props
+
+    return (
+      <form className="server-form" onSubmit={ (evt) => this.onSubmit(evt) }>
+        <h2 className="pane-centered-title">Corpus edition</h2>
+        <div>on { server.name } ({ server.url })</div>
+        <hr />
+        <div className="form-group">
+          <label>Corpus name</label>
+          <input className="form-control"
+                 onChange={ (evt) => this.onChangeName(evt) }
+                 value={ this.state.data.name } />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input className="form-control"
+                 onChange={ (evt) => this.onChangePassword(evt) }
+                 value={ this.state.data.password } />
+        </div>
+        <div className="form-group">
+          <label>Confirm password</label>
+          <input className="form-control"
+                 onChange={ (evt) => this.onChangePasswordConfirm(evt) }
+                 value={ this.state.data.passwordConfirm } />
+        </div>
+        <div className="form-actions">
+          <button className="btn btn-primary" disabled={ this.state.disabled }>Create corpus</button>
+          <Link className="btn btn-default" to="/login">Cancel</Link>
+        </div>
+      </form>
+    )
+  }
 }
 
+CorpusForm.propTypes = {
+  actions: PropTypes.object,
+  server: PropTypes.object
+}
 
+const mapStateToProps = (state) => ({
+  server: state.servers.selected
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(actions, dispatch)
+})
+
+const ConnectedCorpusForm = connect(mapStateToProps, mapDispatchToProps)(CorpusForm)
+
+export default ConnectedCorpusForm
