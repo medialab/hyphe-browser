55d90f91fb94b3afa1960a3c944e45b0458f498c
Nicolas Chambrier <naholyr@gmail.com>
2016-01-13 14:21:39 +0100

    feat(Error): Add timeout for auto-closing errors

--- app/actions/browser.js
+++ app/actions/browser.js
@@ -1,10 +1,16 @@
 export const SHOW_ERROR = '§_SHOW_ERROR'
 export const HIDE_ERROR = '§_HIDE_ERROR'
 
-export const showError = ({ message, fatal, icon = 'alert' }) => ({
-  type: SHOW_ERROR,
-  payload: { message, fatal, icon }
-})
+export const showError = ({ message, fatal, icon = 'alert', timeout = 0 }) => (dispatch) => {
+  dispatch({
+    type: SHOW_ERROR,
+    payload: { message, fatal, icon, timeout }
+  })
+
+  if (timeout) {
+    setTimeout(() => dispatch(hideError()), timeout)
+  }
+}
 
 export const hideError = () => ({
   type: HIDE_ERROR,
