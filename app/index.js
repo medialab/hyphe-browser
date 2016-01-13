370c190a000979ee9640b5468c2fe9f23dd60772
Nicolas Chambrier <naholyr@gmail.com>
2016-01-13 14:41:36 +0100

    chore(index): Skip login form even faster (we don't need the real corpus data)

--- app/index.js
+++ app/index.js
@@ -48,8 +48,7 @@ if (process.env.NODE_ENV === 'development') {
 
 if (process.env.DIRECT_ACCESS_CORPUS_TEST) {
   console.log('Defined environment variable $DIRECT_ACCESS_CORPUS_TEST: switch login form')
-  store.dispatch(require('./actions/servers').fetchCorpora('http://hyphe.medialab.sciences-po.fr/dev-forccast-api')).then((action) => {
-    store.dispatch(require('./actions/corpora').selectCorpus(action.payload.corpora.test))
-    store.dispatch(require('redux-simple-router').pushPath('browser'))
-  })
+  store.dispatch(require('./actions/servers').requestCorpora('http://hyphe.medialab.sciences-po.fr/dev-forccast-api'))
+  store.dispatch(require('./actions/corpora').selectCorpus({ corpus_id: 'test' }))
+  store.dispatch(require('redux-simple-router').pushPath('browser'))
 }
