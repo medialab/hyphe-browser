7eedbc61f8b166d8a4984cbd879728c787e106f3
Delapouite <delapouite@gmail.com>
2016-01-13 14:16:23 +0100

    deps: add redux-localstorage

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import persistState from 'redux-localstorage'
 
 import rootReducer from '../reducers'
 import DevTools from '../components/DevTools' // make it NODE_ENV-dependent
@@ -8,14 +9,19 @@ const finalCreateStore = getCreateStoreModifier()(createStore)
 
 export default (initialState) => finalCreateStore(rootReducer, initialState)
 
-
 function getCreateStoreModifier () {
+  const storage = persistState(['servers'], { key: 'hyphe' })
+
   if (process.env.NODE_ENV === 'development') {
     return compose(
       applyMiddleware(thunk),
+      storage,
       DevTools.instrument()
     )
   } else {
-    return applyMiddleware(thunk)
+    return compose(
+      applyMiddleware(thunk),
+      storage
+    )
   }
 }
