feb6604c1e6b14c0d7c2c348827dc8309aae2bb7
Delapouite <delapouite@gmail.com>
2016-01-14 10:36:14 +0100

    feat(constants): add LOCALES list

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,7 @@
 export const WEBVIEW_UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko'
 export const CORPUS_STATUS_WATCHER_INTERVAL = 5000
+
+export const LOCALES = ['en-US', 'fr-FR']
 export const DEFAULT_LOCALE = 'fr-FR'
 
 export const FORM_CREATE = 'FORM_CREATE'
