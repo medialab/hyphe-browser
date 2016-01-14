ee7ec0272e4c2e2845323ebf1de88d9cc554c74e
Delapouite <delapouite@gmail.com>
2016-01-14 10:56:16 +0100

    feat(reducers): add options reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,17 +2,19 @@ import { combineReducers } from 'redux'
 import { routeReducer } from 'redux-simple-router'
 
 import corpora from './corpora'
+import intl from './intl'
+import options from './options'
 import servers from './servers'
 import tabs from './tabs'
 import ui from './ui'
-import intl from './intl'
 
 export default combineReducers({
   corpora,
+  intl,
+  options,
   servers,
   tabs,
   ui,
-  intl,
   // let redux store the current URL
   routing: routeReducer
 })
