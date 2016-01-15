87339d0a00254efb59ce1e7136d1b78286b1a027
Delapouite <delapouite@gmail.com>
2016-01-15 15:24:18 +0100

    feat(BrowserSideBar): add status buttons

--- app/components/browser/BrowserSideBar.js
+++ app/components/browser/BrowserSideBar.js
@@ -1,3 +1,5 @@
+import '../../css/browser/side-bar'
+
 import React, { PropTypes } from 'react'
 
 import { connect } from 'react-redux'
@@ -5,8 +7,19 @@ import { connect } from 'react-redux'
 class BrowserSideBar extends React.Component {
   render () {
     return (
-      <aside>
-        Sidebar Content
+      <aside className="browser-side-bar">
+        <h2>Status</h2>
+        <div className="btn-group browser-side-bar-status">
+          <button className="btn btn-large btn-default status-in">
+            IN
+          </button>
+          <button className="btn btn-large btn-default status-undecided">
+            ?
+          </button>
+          <button className="btn btn-large btn-default status-out">
+            Out
+          </button>
+        </div>
       </aside>
     )
   }
