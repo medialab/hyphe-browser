26eeff610e01d21f4e7d61dfaa8c04bf0a66f584
Delapouite <delapouite@gmail.com>
2016-02-17 11:10:21 +0100

    feat(BrowserStack): implement elements wireframe

--- app/components/browser/BrowserStack.js
+++ app/components/browser/BrowserStack.js
@@ -1,10 +1,44 @@
+import '../../css/browser/stack'
+
 import React from 'react'
 
 class BrowserStack extends React.Component {
   render () {
     return (
-      <div className="browser-stack">
-        <h2>Stack area (TODO)</h2>
+      <div className="browser-stack toolbar">
+        <div className="browser-stack-filler toolbar-actions">
+          <div className="browser-stack-name">
+            Stack description / name
+          </div>
+          <div className="browser-statck-age">
+            42min old
+            <button className="btn btn-default">
+              Refresh
+              <span className="icon icon-arrows-ccw"></span>
+            </button>
+          </div>
+          <div className="browser-stack-selector">
+            <button className="btn btn-default">Fill</button>
+          </div>
+        </div>
+        <div className="browser-stack-wes toolbar-actions">
+          <div>
+            <button className="btn btn-default browser-stack-wes-prev">
+              <span className="icon icon-left"></span>
+              Previous
+            </button>
+          </div>
+          <div className="browser-stack-wes-selector">
+            <span className="browser-stack-wes-counter">11 in stack</span>
+            <select><option>we -----------------------------</option></select>
+          </div>
+          <div>
+            <button className="btn btn-default browser-stack-wes-next">
+              Next
+              <span className="icon icon-right"></span>
+            </button>
+          </div>
+        </div>
       </div>
     )
   }
