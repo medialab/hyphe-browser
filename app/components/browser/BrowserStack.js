5fafe23ee0ccec0ac92694dee1913dd4be47a752
Delapouite <delapouite@gmail.com>
2016-02-18 10:53:26 +0100

    feat(BrowserStack): add webentity selection triggering setTabUrl

--- app/components/browser/BrowserStack.js
+++ app/components/browser/BrowserStack.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import { FormattedMessage as T, FormattedRelative as D } from 'react-intl'
 
 import { emptyStack, fetchStack } from '../../actions/stacks'
+import { setTabUrl } from '../../actions/tabs'
 
 class BrowserStack extends React.Component {
   constructor (props) {
@@ -20,6 +21,11 @@ class BrowserStack extends React.Component {
     this.props.fetchStack(this.props.server.url, this.props.corpus, stack)
   }
 
+  selectWebentity (weId) {
+    const webentity = this.props.webentities.find(x => x.id === weId)
+    this.props.setTabUrl(webentity.homepage, this.props.activeTabId)
+  }
+
   renderFiller () {
     const { lastRefresh, selectedStack, stacks } = this.props
 
@@ -30,7 +36,7 @@ class BrowserStack extends React.Component {
             <select className="form-control"
               defaultValue={ this.state.selectedStackName }
               onChange={ (evt) => { if (evt.target.value) this.setState({ selectedStackName: evt.target.value }) } }>
-              <option value="">Select a stack</option>
+              <option key="prompt" value="">Select a stack</option>
               { stacks.map(s => (
                 <option key={ s.name } value={ s.name }>{ s.description }</option>
               )) }
@@ -81,9 +87,10 @@ class BrowserStack extends React.Component {
         </div>
         <div className="browser-stack-wes-selector">
           <span className="browser-stack-wes-counter">{ webentities.length } in stack</span>
-          <select>
+          <select className="form-control"
+            onChange={ (evt) => this.selectWebentity(evt.target.value) }>
             { webentities.map(w => (
-              <option id={ w.id }>{ w.name }</option>
+              <option key={ w.id } value={ w.id }>{ w.name } ({ w.homepage })</option>
             )) }
           </select>
         </div>
@@ -108,6 +115,7 @@ class BrowserStack extends React.Component {
 }
 
 BrowserStack.propTypes = {
+  activeTabId: PropTypes.string,
   corpus: PropTypes.object.isRequired,
   lastRefresh: PropTypes.number,
   server: PropTypes.object.isRequired,
@@ -116,10 +124,12 @@ BrowserStack.propTypes = {
   webentities: PropTypes.array.isRequired,
 
   emptyStack: PropTypes.func.isRequired,
-  fetchStack: PropTypes.func.isRequired
+  fetchStack: PropTypes.func.isRequired,
+  setTabUrl: PropTypes.func.isRequired
 }
 
-const mapStateToProps = ({ corpora, servers, stacks }) => ({
+const mapStateToProps = ({ corpora, servers, stacks, tabs }) => ({
+  activeTabId: tabs.activeTab,
   corpus: corpora.selected,
   lastRefresh: stacks.lastRefresh,
   server: servers.selected,
@@ -130,7 +140,8 @@ const mapStateToProps = ({ corpora, servers, stacks }) => ({
 
 const mapDispatchToProps = {
   emptyStack,
-  fetchStack
+  fetchStack,
+  setTabUrl
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BrowserStack)
