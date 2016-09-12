a93495e93f009a3e7f7d4452b2f2b2f207a91098
Delapouite <delapouite@gmail.com>
2016-09-12 15:22:33 +0200

    chore: add webpack-dashboard

--- dev-server.js
+++ dev-server.js
@@ -3,10 +3,13 @@
 var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
+var DashboardPlugin = require('webpack-dashboard/plugin')
 var config = require('./webpack.config.development')
 
 var app = express()
 var compiler = webpack(config)
+
+compiler.apply(new DashboardPlugin())
 
 app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath,
