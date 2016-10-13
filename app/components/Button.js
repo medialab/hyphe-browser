eee6505efd2db568a3e70a228dceed62d13d4f52
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2016-10-13 18:11:16 +0200

    avoid tooltips bottom/top on buttons being always left

--- app/components/Button.js
+++ app/components/Button.js
@@ -4,10 +4,11 @@ import cx from 'classnames'
 class Button extends React.Component {
   render () {
     const { icon, onClick, disabled = false, title, className = '' } = this.props
+    const classes = className + (!~className.indexOf("hint--") && title ? " hint--left" : "")
     const props = { disabled, onClick}
 
     return (
-      <button className={ cx("btn btn-default hint--left", className) } { ...props } aria-label={ title }>
+      <button className={ cx("btn btn-default", classes) } { ...props } aria-label={ title }>
         <span className={ 'ti-' + icon }></span>
       </button>
     )
