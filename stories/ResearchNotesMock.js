55fa0f1d52402adce56c0b6a44af1b92ab520f0e
Robin de Mourat <robin.demourat@gmail.com>
2019-05-27 12:02:43 +0200

    sketch research notes proposal

---
+++ stories/ResearchNotesMock.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import cx from 'classnames'
+
+import Textarea from 'react-autosize-textarea'
+
+import HelpPin from '../app/components/HelpPin'
+import Button from '../app/components/Button'
+
+import './ResearchNotes.styl'
+
+
+const ResearchNotesMock = function (){
+  const [open, setOpen] = useState(true)
+  const [textAreaText, setTextAreaText] = useState('')
+  const [notes, setNotes] = useState([])
+  const onAddNote = (e) => {
+    if(textAreaText.length) {
+      setNotes([textAreaText, ...notes])
+      setTextAreaText('')
+    }
+    e.preventDefault()
+    e.stopPropagation()
+      
+  }
+  return (
+    <div className="browser-side-bar">
+      <div className="browser-side-bar-sections">
+  
+        <div className="browser-side-bar-tags browser-research-notes">
+          <div>
+            <h3
+              onClick={ () => setOpen(!open) }
+            >
+              <span 
+                className={ cx({
+                  'ti-angle-up': open,
+                  'ti-angle-down': !open
+                }) }
+              />
+              <span>Notes <HelpPin>about notes</HelpPin></span>
+                
+            </h3>
+  
+            {
+              open &&
+              <div>
+                <form onSubmit={ onAddNote } className="browser-side-bar-tags-new-category">
+                  <Textarea value={ textAreaText } onChange={ e => setTextAreaText(e.target.value) } />
+                  <button 
+                    className={ cx({
+                      'add-button': true,
+                      'is-disabled': !textAreaText.length
+                    }) } 
+                    disabled={ !textAreaText.length }
+                    type="submit" 
+                    onClick={ (e) => {onAddNote(e)} } 
+                  >
+                        Add note
+                  </button>
+                </form>
+                {
+                  notes.length ?
+                    <h5>Existing notes</h5>
+                    : null
+                } 
+                {
+                  notes.map((note, index) => {
+                    const onRemove = () => {
+                      setNotes(notes.filter((n, i) => i !== index))
+                    }
+                    return (
+                      <div key={ index } className="research-note">
+                        <div className="research-note-content">{
+                            note.split('\n').map((i, key) => <div key={key}>{i}</div>)
+                            }</div>
+                        <Button icon="close" onClick={ onRemove }>x</Button>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ResearchNotesMock
--- stories/index.js
+++ stories/index.js
@@ -19,15 +19,13 @@ import './stories.styl'
 /**
  * JS libs import
  */
-import React, {useState} from 'react'
-import cx from 'classnames'
+import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 
 /**
  *  Components import
  */
-import HelpPin from '../app/components/HelpPin'
 // import Button from '../app/components/Button'
 
 /**
@@ -39,42 +37,12 @@ import HelpPin from '../app/components/HelpPin'
 /**
  * Help pin
  */
- const PinExample = function(){
-  const [open, setOpen] = useState(true)
-  return (
-    <div className="browser-side-bar">
-      <div className="browser-side-bar-sections">
-
-        <div className="browser-side-bar-tags">
-          <div>
-            <h3
-              onClick={() => setOpen(!open)}
-            >
-              <span 
-                className={ cx({
-                  'ti-angle-up': open,
-                  'ti-angle-down': !open
-                }) } />
-              <span>Tags <HelpPin>about tags</HelpPin></span>
-              
-            </h3>
-
-            {
-              open &&
-              <form className="browser-side-bar-tags-new-category">
-                {/* <input 
-                  placeholder={ 'add category' }
-                  value={ '' }
-                />
-                <Button icon="plus" /> */}
-                tags contents
-              </form>
-            }
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-storiesOf('Help pin (embedded documentation)')
+import PinExample from './PinExample';
+storiesOf('Help pin (embedded documentation)', module)
   .add('example', () => <PinExample />)
+/**
+ * Research notes
+ */
+import ResearchNotesMock from './ResearchNotesMock';
+storiesOf('Research notes', module)
+  .add('Sketch', () => <ResearchNotesMock />)
