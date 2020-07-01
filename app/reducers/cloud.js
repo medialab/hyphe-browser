b1f2d4ff40e4fd15741e38adc6488ddaac33bfbe
Alexis Jacomy <alexis.jacomy@gmail.com>
2020-07-01 16:41:11 +0200

    Removes unused server logic from 'cloud' actions/reducers

--- app/reducers/cloud.js
+++ app/reducers/cloud.js
@@ -2,9 +2,6 @@ import { omit } from 'lodash'
 
 import createReducer from '../utils/create-reducer'
 import {
-  CREATE_CLOUD_SERVER,
-  UPDATE_CLOUD_SERVER,
-  DELETE_CLOUD_SERVER,
   SAVE_CREDENTIALS,
   DELETE_CREDENTIALS,
   CLEAR_CREDENTIALS,
@@ -12,32 +9,10 @@ import {
 } from '../actions/cloud'
 
 const initialState = {
-  credentials: {},
-  serversList: []
+  credentials: {}
 }
 
 export default createReducer(initialState, {
-  // a server has been created from the "Create cloud server" form
-  [CREATE_CLOUD_SERVER]: (state, { cloudServer }) => ({
-    ...state,
-    serversList: state.serversList.concat([cloudServer])
-  }),
-
-  // a server has been updated from the cloud servers administration page (TODO)
-  [UPDATE_CLOUD_SERVER]: (state, { cloudServer }) => ({
-    ...state,
-    serversList: state.serversList.map(server => {
-      return server.id === cloudServer.id ? cloudServer : server
-    })
-  }),
-
-  // a server has been deleted from the cloud servers administration page (TODO)
-  [DELETE_CLOUD_SERVER]: (state, { cloudServer }) => ({
-    ...state,
-    serversList: state.serversList.filter(server => server.id !== cloudServer.id)
-
-  }),
-
   // the user has used new OpenStack credentials for a given host when creating a cloud server
   [SAVE_CREDENTIALS]: (state, { host, credentials }) => ({
     ...state,
@@ -47,18 +22,18 @@ export default createReducer(initialState, {
     }
   }),
 
-  // the user has chosen to forget his OpenStack credentials for a given host (TODO)
+  // the user has chosen to forget his OpenStack credentials for a given host
   [DELETE_CREDENTIALS]: (state, { host }) => ({
     ...state,
     credentials: omit(state.credentials, [host])
   }),
 
-  // the user has chosen to forget all his OpenStack credentials (TODO)
+  // the user has chosen to forget all his OpenStack credentials
   [CLEAR_CREDENTIALS]: (state) => ({
     ...state,
     credentials: {}
   }),
 
-  // the user has chosen to forget every cloud related data (TODO)
+  // the user has chosen to forget every cloud related data
   [RESET_ALL_CLOUD_DATA]: () => initialState,
 })
