051ae98ec15acd1e73c9979e8a4ae80959e0b7ef
mydu <dumengying1989@gmail.com>
2020-09-08 14:53:22 +0200

    refactor CorpusForm #171

--- app/containers/Login/CorpusForm.js
+++ app/containers/Login/CorpusForm.js
@@ -3,114 +3,106 @@
 // local validation errors :
 // - password must equals passwordConfirm
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { FormattedMessage as T, injectIntl } from 'react-intl'
+import { FormattedMessage as T, useIntl } from 'react-intl'
 
 import { createCorpus } from '../../actions/corpora'
 import Spinner from '../../components/Spinner'
 import HelpPin from '../../components/HelpPin'
 
 const creationRules = ['domain', 'subdomain', 'page']
-class CorpusForm extends React.Component {
 
-  // generic form methods
+const CorpusForm = ({
+  serverStatus,
+  server,
+  serverError,
+  location,
+  createCorpus
+}) => {
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      submitting: false,
-      error: null,
-      data: this.getInitData(props),
-      passwordProtected: false,
-      advancedOptions: false,
-      crawlDepth: 1,
-      creationRule: 'domain'
-    }
-  }
+  const [submitting, setSubmitting] = useState(false)
+  const [error, setError] = useState(null)
+  const [data, setData] = useState({
+    name: location.state && location.state.filterName,
+    password: null,
+    passwordConfirm: null
+  })
+  const [passwordProtected, setPasswordProtected] = useState(false)
+  const [advancedOptions, setAdvancedOptions] = useState(false)
+  const [crawlDepth, setCrawlDepth] = useState(1)
+  const [creationRule, setCreationRule] = useState('domain')
 
-  componentWillReceiveProps ({ serverError }) {
+  const maxDepth = serverStatus && serverStatus.hyphe && serverStatus.hyphe.max_depth || 3
+  const depths = [ ...Array(maxDepth).keys() ].map(i => i+1)
+  const { formatMessage } = useIntl()
+
+  useEffect(() => {
     if (serverError && serverError.messageId && serverError.messageId.includes('error.corpus-not-created')) {
-      this.setState({
-        submitting: false,
-        error: serverError
-      })
+      setSubmitting(false)
+      setError(serverError)
     }
-  }
+  }, [serverError])
 
   // deal with fields values
-  setDataState (key, value) {
-    const data = {
-      ...this.state.data,
+  const setDataState = (key, value) => {
+    setData({
+      ...data,
       [key]: value
-    }
-    this.setState({ data })
+    })
   }
 
-  renderFormGroup (name, label = name, type = 'text', autoFocus = false) {
-    const { intl: { formatMessage } } = this.props
+  const renderFormGroup = (name, label = name, type = 'text', autoFocus = false) => {
     return (
       <div className="form-group">
         <label><T id={ label } /></label>
         <input
-          disabled={ this.state.submitting }
+          disabled={ submitting }
           autoFocus={ autoFocus }
           name={ name }
           placeholder={ formatMessage({ id: label }) }
-          onChange={ ({ target }) => this.setDataState(name, target.value) }
+          onChange={ ({ target }) => setDataState(name, target.value) }
           type={ type }
-          value={ this.state.data[name] || '' }
+          value={ data[name] || '' }
         />
       </div>
     )
   }
 
-  getInitData (props) {
-    const { location } = props
-    return {
-      name: location.state && location.state.filterName,
-      password: null,
-      passwordConfirm: null
-    }
-  }
-
-  onSubmit = (evt) => {
+  const onSubmit = (evt) => {
     // no real submit to the server
     evt.preventDefault()
-    const newState = {
-      submitting: true,
-      error: null
-    }
 
-    if (!this.isValid()) {
-      newState.submitting = false
-      newState.error = { messageId:'error.password-mismatch' }
-      newState.data = {
-        ...this.state.data,
+    if (!isValid()) {
+      setSubmitting(false)
+      setError({ messageId: 'error.password-mismatch' })
+      setData({
+        ...data,
         password: '',
         passwordConfirm: ''
-      }
-      return this.setState(newState)
+      })
+      return
     }
-    this.setState(newState)
+    setSubmitting(true)
+    setError(null)
 
-    const corpus = this.cleanData()
-    this.props.createCorpus({
-      server: this.props.server,
+    const corpus = cleanData()
+    createCorpus({
+      server,
       corpus,
       options: {
-        depthHypheBro: this.state.crawlDepth,
-        defaultCreationRule: this.state.creationRule
+        depthHypheBro: crawlDepth,
+        defaultCreationRule: creationRule
       }
     })
   }
 
-  cleanData () {
+  const cleanData = () => {
     const corpus = {
-      ...this.state.data
+      ...data
     }
     delete corpus.passwordConfirm
     if (!corpus.name) delete corpus.name
@@ -118,128 +110,119 @@ class CorpusForm extends React.Component {
     return corpus
   }
 
-  isValid () {
-    return this.state.data.password === this.state.data.passwordConfirm
+  const isValid = () => {
+    return data.password === data.passwordConfirm
   }
 
-  render () {
-    const { error, passwordProtected, advancedOptions, creationRule, crawlDepth } = this.state
-    const { serverStatus } = this.props
-    const maxDepth = serverStatus && serverStatus.hyphe && serverStatus.hyphe.max_depth || 3
-    const depths = [ ...Array(maxDepth).keys() ].map(i => i+1)
-
-    const onTogglePasswordProtected = () => {
-      let newState = {
-        passwordProtected: !passwordProtected
-      }
-      // if disabling password protected clear the password
-      if (passwordProtected) {
-        newState = {
-          ...newState,
-          password: '',
-          passwordConfirm: ''
-        }
-      }
-      this.setState(newState)
+  const onTogglePasswordProtected = () => {
+    // if disabling password protected clear the password
+    if (passwordProtected) {
+      setData({
+        ...data,
+        password: '',
+        passwordConfirm: ''
+      })
     }
-    return (
-      <form className="corpus-form" onSubmit={ this.onSubmit }>
-        <h3 className="section-header">
-          <T id="create-a-corpus" />
-        </h3>
-        { error &&
-          <div className="form-error"><T id={ error.messageId } values={ error.messageValues || {} } /></div>
-        }
-
-        <div className="config-form">
-          { this.state.submitting ?
-            <h5>{this.state.data.name}</h5>
-            :
-            this.renderFormGroup('name', 'corpus-name', 'text', true)
-          }
-          <div className={ cx('options-wrapper', { active: passwordProtected }) }>
-            {
-              !this.state.submitting &&
-              <div onClick={ onTogglePasswordProtected } className="form-group horizontal">
-                <input readOnly checked={ passwordProtected } type="radio" />
-                <label><T id="password-protected-creation" /></label>
-              </div>
-            }
-
-            { passwordProtected && this.renderFormGroup('password', 'password', 'password') }
-            { passwordProtected && this.renderFormGroup('passwordConfirm', 'confirm-password', 'password') }
-          </div>
-          <div className={ cx('options-wrapper', { active: advancedOptions }) }>
-            {!this.state.submitting &&
-              <div onClick={ () => this.setState({ advancedOptions: !advancedOptions }) } className="form-group horizontal">
-                <input readOnly checked={ advancedOptions } type="radio" />
-                <label><T id="advanced-creation-options" /></label>
-              </div>
-            }
-            {advancedOptions &&
-            <>
-              <div className="form-group">
-                <label><T id="depth-creation" />
-                  <HelpPin place="top">
-                    <T id="depth-creation-help" />
-                  </HelpPin>
-                </label>
-                {
-                  depths.map((depth, index) => (
-                    <div
-                      key={ index }
-                      className="form-group horizontal minified"
-                      onClick={ () => this.setState({ crawlDepth: depth }) }
-                    >
-                      <input readOnly type="radio" checked={ depth === crawlDepth } />
-                      <label>{depth}</label>
-                    </div>
-                  ))
-                }
-              </div>
-              <div className="form-group">
-                <label><T id="default-creation-rule" />
-                  <HelpPin place="top">
-                    <T id="default-creation-rule-help" />
-                  </HelpPin>
-
-                </label>
-                {
-                  creationRules.map((rule, index) => (
-                    <div
-                      key={ index }
-                      className="form-group horizontal minified"
-                      onClick={ () => this.setState({ creationRule: rule }) }
-                    >
-                      <input readOnly type="radio" checked={ creationRule === rule } />
-                      <label>{rule}</label>
-                    </div>
-                  ))
-                }
-              </div>
-            </>
-            }
-          </div>
-        </div>
-
-        { this.state.submitting
-          ? <Spinner />
-          : (
-            <div className="buttons-row">
-              <li>
-                <Link className="btn btn-error" to="/login"><T id="cancel" /></Link>
-              </li>
-              <li className="main-button-container">
-                <button className={ cx('btn btn-primary', { 'is-disabled': !this.state.data.name }) } disabled={ this.state.submitting }>
-                  <T id="create-corpus" />
-                </button>
-              </li>
-            </div>
-          )
-        }
-      </form>
-    )
+    setPasswordProtected(!passwordProtected)
   }
+
+  return (
+    <form className="corpus-form" onSubmit={ onSubmit }>
+      <h3 className="section-header">
+        <T id="create-a-corpus" />
+      </h3>
+      { error &&
+        <div className="form-error"><T id={ error.messageId } values={ error.messageValues || {} } /></div>
+      }
+
+      <div className="config-form">
+        { submitting ?
+          <h5>{data.name}</h5>
+          :
+          renderFormGroup('name', 'corpus-name', 'text', true)
+        }
+        <div className={ cx('options-wrapper', { active: passwordProtected }) }>
+          {
+            !submitting &&
+            <div onClick={ onTogglePasswordProtected } className="form-group horizontal">
+              <input readOnly checked={ passwordProtected } type="radio" />
+              <label><T id="password-protected-creation" /></label>
+            </div>
+          }
+
+          { passwordProtected && renderFormGroup('password', 'password', 'password') }
+          { passwordProtected && renderFormGroup('passwordConfirm', 'confirm-password', 'password') }
+        </div>
+        <div className={ cx('options-wrapper', { active: advancedOptions }) }>
+          {!submitting &&
+            <div onClick={ () => setAdvancedOptions(!advancedOptions) } className="form-group horizontal">
+              <input readOnly checked={ advancedOptions } type="radio" />
+              <label><T id="advanced-creation-options" /></label>
+            </div>
+          }
+          {advancedOptions &&
+          <>
+            <div className="form-group">
+              <label><T id="depth-creation" />
+                <HelpPin place="top">
+                  <T id="depth-creation-help" />
+                </HelpPin>
+              </label>
+              {
+                depths.map((depth, index) => (
+                  <div
+                    key={ index }
+                    className="form-group horizontal minified"
+                    onClick={ () => setCrawlDepth(depth) }
+                  >
+                    <input readOnly type="radio" checked={ depth === crawlDepth } />
+                    <label>{depth}</label>
+                  </div>
+                ))
+              }
+            </div>
+            <div className="form-group">
+              <label><T id="default-creation-rule" />
+                <HelpPin place="top">
+                  <T id="default-creation-rule-help" />
+                </HelpPin>
+
+              </label>
+              {
+                creationRules.map((rule, index) => (
+                  <div
+                    key={ index }
+                    className="form-group horizontal minified"
+                    onClick={ () => setCreationRule(rule) }
+                  >
+                    <input readOnly type="radio" checked={ creationRule === rule } />
+                    <label>{rule}</label>
+                  </div>
+                ))
+              }
+            </div>
+          </>
+          }
+        </div>
+      </div>
+
+      { submitting
+        ? <Spinner />
+        : (
+          <div className="buttons-row">
+            <li>
+              <Link className="btn btn-error" to="/login"><T id="cancel" /></Link>
+            </li>
+            <li className="main-button-container">
+              <button className={ cx('btn btn-primary', { 'is-disabled': !data.name }) } disabled={ submitting }>
+                <T id="create-corpus" />
+              </button>
+            </li>
+          </div>
+        )
+      }
+    </form>
+  )
 }
 
 CorpusForm.propTypes = {
@@ -262,4 +245,4 @@ const ConnectedCorpusForm = connect(mapStateToProps, {
   createCorpus
 })(CorpusForm)
 
-export default injectIntl(ConnectedCorpusForm)
+export default ConnectedCorpusForm
